a89fef40b42f8d4f22dc765c1e02ed638af0ab9c
ElizabethBrooks <merimaat1@gmail.com>
2024-05-24 11:28:24 -0400

    Created separate species data prep scripts

--- dataPrep/prepareReads_obtusa.sh
+++ dataPrep/prepareReads_obtusa.sh
@@ -2,23 +2,20 @@
 #$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -r n
-#$ -N prepData_projects_jobOutput
+#$ -N prepData_obtusa_jobOutput
 
 # script to perform data pre-processing and prepare RNAseq reads for analysis
-# usage: qsub prepareReads.sh
+# usage: qsub prepareReads_obtusa.sh
 
 # set input paths
 obtusaInPathOne="/afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/Daphnia_RNAseq/D_obtusa/D_obtusa_multi/multi_condition_BGI_DNBSeq/F20FTSUSAT1134_DAPfhqR/RawData"
 obtusaInPathTwo="/afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/Daphnia_RNAseq/D_obtusa/D_obtusa_multi/multi_condition_BGI_DNBSeq/F20FTSUSAT1134_DAPltkR/RawData/"
-pulicariaInPath="/afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/Daphnia_RNAseq/D_pulicaria/D_pulicaria_Lk16_Transcriptome/Lk16_Pool1"
 
 # retrieve output paths
 obtusaOutPath=$(grep "pairedReads:" ../"inputData/inputPaths_obtusa.txt" | tr -d " " | sed "s/pairedReads://g")
-pulicariaOutPath=$(grep "pairedReads:" ../"inputData/inputPaths_pulicaria.txt" | tr -d " " | sed "s/pairedReads://g")
 
 # create output directories
 mkdir $obtusaOutPath
-mkdir $pulicariaOutPath
 
 # status message
 echo "Beginning data prep ..."
@@ -45,28 +42,6 @@ for i in $obtusaInPathOne"/"*/*_2.fq.gz; do
 	inName=$(basename $i | sed "s/_2\.fq\.gz//g" | sed "s/_/-/g")
 	# combine files for read 2
 	cat $i $obtusaInPathTwo"/"$inName"/"*_2.fq.gz > $obtusaOutPath"/"$outName
-done
-
-# pulicaria
-# read 1
-for i in $pulicariaInPath"/"*; do 
-	# setup output name
-	outName=$(ls $i"/"*_R1_*.fastq.gz | head -1)
-	outName=$(basename $outName | sed "s/_001//g")
-	# status message
-	echo "Processing $outName ..."
-	# combine files for read 1
-	cat $i"/"*_R1_*.fastq.gz > $pulicariaOutPath"/"$outName
-done
-# read 2
-for i in $pulicariaInPath"/"*; do 
-	# setup output name
-	outName=$(ls $i"/"*_R2_*.fastq.gz | head -1)
-	outName=$(basename $outName | sed "s/_002//g")
-	# status message
-	echo "Processing $outName ..."
-	# combine files for read 2
-	cat $i"/"*_R2_*.fastq.gz > $pulicariaOutPath"/"$outName
 done
 
 # status message
---
+++ dataPrep/prepareReads_pulicaria.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N prepData_pulicaria_jobOutput
+
+# script to perform data pre-processing and prepare RNAseq reads for analysis
+# usage: qsub prepareReads_pulicaria.sh
+
+# set input paths
+pulicariaInPathOne="/afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/Daphnia_RNAseq/D_pulicaria/D_pulicaria_Lk16_Transcriptome/Lk16_Pool1"
+pulicariaInPathTwo="/afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/Daphnia_RNAseq/D_pulicaria/D_pulicaria_Lk16_Transcriptome/Lk16_Pool2"
+
+# retrieve output paths
+pulicariaOutPath=$(grep "pairedReads:" ../"inputData/inputPaths_pulicaria.txt" | tr -d " " | sed "s/pairedReads://g")
+
+# create output directories
+mkdir $pulicariaOutPath
+
+# status message
+echo "Beginning data prep ..."
+
+# pulicaria
+# data set 1
+# read 1
+for i in $pulicariaInPathOne"/"*; do 
+	# setup output name
+	outName=$(ls $i"/"*_R1_*.fastq.gz | head -1)
+	outName=$(basename $outName | sed "s/_001//g")
+	# status message
+	echo "Processing $outName ..."
+	# combine files for read 1
+	cat $i"/"*_R1_*.fastq.gz > $pulicariaOutPath"/"$outName
+done
+# read 2
+for i in $pulicariaInPathOne"/"*; do 
+	# setup output name
+	outName=$(ls $i"/"*_R2_*.fastq.gz | head -1)
+	outName=$(basename $outName | sed "s/_001//g")
+	# status message
+	echo "Processing $outName ..."
+	# combine files for read 2
+	cat $i"/"*_R2_*.fastq.gz  > $pulicariaOutPath"/"$outName
+done
+# data set 2
+# read 1
+for i in $pulicariaInPathTwo"/"*; do 
+	# setup output name
+	outName=$(ls $i"/"*_R1_*.fastq.gz | head -1)
+	outName=$(basename $outName | sed "s/_001//g")
+	# status message
+	echo "Processing $outName ..."
+	# combine files for read 1
+	cat $i"/"*_R1_*.fastq.gz > $pulicariaOutPath"/"$outName
+done
+# read 2
+for i in $pulicariaInPathTwo"/"*; do 
+	# setup output name
+	outName=$(ls $i"/"*_R2_*.fastq.gz | head -1)
+	outName=$(basename $outName | sed "s/_001//g")
+	# status message
+	echo "Processing $outName ..."
+	# combine files for read 2
+	cat $i"/"*_R2_*.fastq.gz  > $pulicariaOutPath"/"$outName
+done
+
+# status message
+echo "Data prep completed!"
