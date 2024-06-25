71e20483ee00bfff89dae4eb875a7eb51e1efb56
ElizabethBrooks <merimaat1@gmail.com>
2024-06-24 20:12:15 -0400

    Added RBH gene ID formatting

--- Orthology/Local/scripts/find_RBH.sh
+++ Orthology/Local/scripts/find_RBH.sh
@@ -7,14 +7,15 @@
 firstBlast=$1
 secondBlast=$2
 
-# setup output file path
-outputFile=$(echo $firstBlast | sed "s/\.blast/_RBH.csv/g")
+# setup output file paths
+outputProt=$(echo $firstBlast | sed "s/\.blast/_RBH_proteins.csv/g")
+outputGenes=$(echo $firstBlast | sed "s/\.blast/_RBH_genes.csv/g")
 
 # retrieve blastp comparison tags
 comparison=$(basename $firstBlast | sed "s/\.blast//g")
 
 # add header in csv format
-echo $comparison | sed "s/_/,/g" > $outputFile
+echo $comparison | sed "s/_/,/g" > $outputProt
 
 # status message
 echo "Finding RBHs..."
@@ -24,9 +25,12 @@ while IFS=$'\t' read -r f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12
 do
 	# determine annotation sets
 	if grep -q "$f2"$'\t'"$f1"$'\t' $secondBlast; then #RBH
-		echo "$f1,$f2" >> $outputFile
+		echo "$f1,$f2" >> $outputProt
 	fi
 done < $firstBlast
+
+# convert transcript IDs to gene IDs and remove duplicate entries
+cat $outputFile | sed "s/\.t.//g" | awk -F, '!seen[$1]++' | awk -F, '!seen[$2]++' > $outputGenes
 
 # status message
 echo "RBHs Found!"
